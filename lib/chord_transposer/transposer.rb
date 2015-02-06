be35d2547d3660d4866f79d27c73bc09f11a3474
Nica <nicanorperera@gmail.com>
2015-02-05 21:20:37 -0300

    Added Second Test. All mayor notes can be transposed 1 tone.

--- lib/chord_transposer/transposer.rb
+++ lib/chord_transposer/transposer.rb
@@ -1,11 +1,15 @@
 module ChordTransposer
   class Transposer
 
+    CHORDS = ['Do', 'Do#', 'Re', 'Re#', 'Mi', 'Fa', 'Fa#', 'Sol', 'Sol#', 'La', 'La#', 'Si']
+
+    
     def initialize(tranpose=0)
     end
 
     def transpose(chord)
-      're'
+      index = CHORDS.index(chord)
+      CHORDS[(index + 2)%12]
     end
   end
 end
--- spec/chord_transposer_spec.rb
+++ spec/chord_transposer_spec.rb
@@ -5,7 +5,17 @@ describe ChordTransposer do
   describe ".transpose" do
     it "changes 'do' to 're'" do
       transposer = ChordTransposer::Transposer.new(2)
-      expect(transposer.transpose('do')).to eq('re')
+      expect(transposer.transpose('Do')).to eq('Re')
     end
+
+    it "transposes all 12 mayor notes one tone higher" do
+      transposer = ChordTransposer::Transposer.new(2)
+      original_chords = ['Do', 'Do#', 'Re', 'Re#', 'Mi', 'Fa', 'Fa#', 'Sol', 'Sol#', 'La', 'La#', 'Si']
+      expected_chords = ['Re', 'Re#', 'Mi', 'Fa', 'Fa#', 'Sol', 'Sol#', 'La', 'La#', 'Si', 'Do', 'Do#']
+      (0..11).each do |index|
+        expect(transposer.transpose(original_chords[index])).to eq(expected_chords[index])
+      end
+    end
+
   end
 end
